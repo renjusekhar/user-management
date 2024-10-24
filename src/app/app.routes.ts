121f2d157e68ebf3408b6669c155ddbb83302b51
renjusekhar <renjusekhar@MacBook.local>
2024-10-24 12:45:48 +0530

    updated lazy modules in the route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
-import { UserTableViewComponent } from './usermanagement/user-table-view/user-table-view.component';
-import { UserDetailsViewComponent } from './usermanagement/user-details-view/user-details-view.component';
-import { NoDataFoundComponent } from './no-data-found/no-data-found.component';
 
 export const routes: Routes = [
-  { path: 'user/:id', component: UserDetailsViewComponent },
-  { path: 'table-view', component: UserTableViewComponent },
-  { path: '', component: UserTableViewComponent },
-  { path: '**', component: NoDataFoundComponent },
+  {
+    path: 'user/:id',
+    loadComponent: () => import('./usermanagement/user-details-view/user-details-view.component').then(m => m.UserDetailsViewComponent)
+  },
+  {
+    path: 'table-view',
+    loadComponent: () => import('./usermanagement/user-table-view/user-table-view.component').then(m => m.UserTableViewComponent)
+  },
+  {
+    path: '',
+    loadComponent: () => import('./usermanagement/user-table-view/user-table-view.component').then(m => m.UserTableViewComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./no-data-found/no-data-found.component').then(m => m.NoDataFoundComponent)
+  }
 ];
-
-
